2533bd7715b53c2cfdc4e2e856d6cb79ba7daea1
zlantobi8 <151410039+zlantobi8@users.noreply.github.com>
2025-02-25 13:01:20 +0100

    Update index.js

--- index.js
+++ index.js
@@ -538,7 +538,7 @@ const response1 =
                     "plan_network": "MTN",
                     "month_validate": "30 Days",
                     "plan": "500MB",
-                    "plan_amount": "340"
+                    "plan_amount": "330"
                 },
                 {
                     "id": 2,
