a201ee24b09d13e8d22a965f7216d18019b19aa3
zlantobi8 <151410039+zlantobi8@users.noreply.github.com>
2025-03-23 15:40:52 +0100

    Update index.js

--- index.js
+++ index.js
@@ -667,7 +667,7 @@ const response1 =
                     "plan_network": "AIRTEL",
                     "month_validate": "30 Days",
                     "plan": "500MB",
-                    "plan_amount": "340.00"
+                    "plan_amount": "350.00"
                 },
                 {
                     "id": 29,
@@ -677,7 +677,7 @@ const response1 =
                     "plan_network": "AIRTEL",
                     "month_validate": "30 Days",
                     "plan": "1GB",
-                    "plan_amount": "670.00"
+                    "plan_amount": "700.00"
                 },
                 {
                     "id": 30,
@@ -687,7 +687,7 @@ const response1 =
                     "plan_network": "AIRTEL",
                     "month_validate": "30 Days",
                     "plan": "2GB",
-                    "plan_amount": "1330.00"
+                    "plan_amount": "1400.00"
                 },
                 {
                     "id": 31,
@@ -707,7 +707,7 @@ const response1 =
                     "plan_network": "AIRTEL",
                     "month_validate": "30 Days",
                     "plan": "5GB",
-                    "plan_amount": "3275.00"
+                    "plan_amount": "4500.00"
                 },
                 {
                     "id": 33,
@@ -717,7 +717,7 @@ const response1 =
                     "plan_network": "AIRTEL",
                     "month_validate": "30 Days",
                     "plan": "10GB",
-                    "plan_amount": "6450.00"
+                    "plan_amount": "7000.00"
                 }
             ]
         },
